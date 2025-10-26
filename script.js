70bf5d72cf7e6119470c3d33da9063a16832256f
WindBreaker1 <dokgojungwoo@gmail.com>
2025-10-26 09:34:30 +0200

    decided to use footer for menu

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 // ------------------------------------------ //
 // Hide/Show Aside //
 
-document.addEventListener('DOMContentLoaded', function() {
-  let aside = document.querySelector(".aside");
-  let mainPage = document.querySelector(".main-page");
-  let menuIcon = document.querySelector(".menu-icon");
-
-  if (!menuIcon || !aside || !mainPage) {
-    console.error("Menu elements not found");
-    return;
-  }
-
-  menuIcon.addEventListener("click", () => {
-    aside.classList.toggle("hidden");
-    mainPage.classList.toggle("expanded");
-  });
-});
+// easter egg 🥚
